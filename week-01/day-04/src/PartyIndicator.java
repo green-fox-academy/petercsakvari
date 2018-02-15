e42dab6638bd55a5d3b99d5cef8f266b0e7a6ab3
Peter Csakvari <csakvaripeter1@gmail.com>
2018-02-15 16:51:31 +0100

    feat(javatasks)PartyIndicator

---
+++ week-01/day-04/src/PartyIndicator.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class PartyIndicator {
+    public static void main(String[] args) {
+
+        // Write a program that asks for two numbers
+        // The first number represents the number of girls that comes to a party, the
+        // second the boys
+        //
+        // It should print: The party is excellent!
+        // If the the number of girls and boys are equal and there are more people coming than 20
+        //
+        // It should print: Quite cool party!
+        // It there are more than 20 people coming but the girl - boy ratio is not 1-1
+        //
+        // It should print: Average party...
+        // If there are less people coming than 20
+        //
+        // It should print: Sausage party
+        // If no girls are coming, regardless the count of the people
+
+        System.out.println("Write the number of girls then the number of boys: ");
+        Scanner scanner = new Scanner(System.in);
+        int girls = scanner.nextInt();
+        int boys = scanner.nextInt();
+        if ((girls == boys) && ((girls + boys) > 20)){
+            System.out.println("The party is excellent!");
+        } else if ((girls != boys) && ((girls + boys) > 20)){
+            System.out.println("Quite cool party!");
+        } else if ((girls + boys) < 20){
+            System.out.println("Average party...");
+        } else (girls == 0){
+            System.out.println("Sausage party");
+        }
+
+
+
+
+    }
+}
