1995871d45a0e0a850c4e2a2b98ea5681e30ea55
Peter Csakvari <csakvaripeter1@gmail.com>
2018-02-15 12:19:42 +0100

    feat(task1-3): tasks completed

--- week-01/day-04/src/task01.java
+++ week-01/day-04/src/task01.java
@@ -1,6 +1,15 @@
 public class task01 {
     public static void main(String[] args){
 
-        
+        System.out.println("Hello, Peter!");
+
+        System.out.println("Humpty Dumpty sat on a wall,");
+        System.out.println("Humpty Dumpty had a great fall.");
+        System.out.println("All the king's horses and all the king's men");
+        System.out.println("Couldn't put Humpty together again.");
+
+        System.out.println("Hello, Dominik!");
+        System.out.println("Hello, Denes!");
+        System.out.println("Hello, Laci!");
     }
 }
