480f386fdba06a826d2bf2b8bf448242631169bb
Peter Csakvari <csakvaripeter1@gmail.com>
2018-02-15 17:17:47 +0100

    feat(javatasks)IWontCheatOnExams

---
+++ week-01/day-04/src/IWontCheatOnExams.java
@@ -0,0 +1,7 @@
+public class IWontCheatOnExams {
+    public static void main(String[] args){
+        for (int i = 0; i < 100; i++){
+            System.out.println("I wont cheat on the exam!");
+        }
+    }
+}
