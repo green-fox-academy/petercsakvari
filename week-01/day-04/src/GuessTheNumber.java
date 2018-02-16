f5f76c4bbd6d8b2eced0b94221f38e4f183d82ac
Peter Csakvari <csakvaripeter1@gmail.com>
2018-02-16 14:39:08 +0100

    fix(javatasks)GuessTheNumber

--- week-01/day-04/src/GuessTheNumber.java
+++ week-01/day-04/src/GuessTheNumber.java
@@ -1,31 +1,36 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class GuessTheNumber {
     public static void main(String[] args){
-        // Write a program that stores a number, and the user has to figure it out.
-        // The user can input guesses, after each guess the program would tell one
-        // of the following:
-        //
-        // The stored number is higher
-        // The stried number is lower
-        // You found the number: 8
 
         Scanner scanner = new Scanner(System.in);
-
-        int number = 7;
+        Random rand = new Random();
+        int number = rand.nextInt(10) + 1;
         boolean check = true;
 
         while (check){
-            System.out.println("Guess a number: ");
-            int guess = scanner.nextInt();
-                if (guess < number){
-                    System.out.println("The stored number is higher");
-                } else if (guess > number){
-                    System.out.println("The stored number is lower");
-                } else {
-                    check = false;
-                    System.out.println("You found the number: " + number);
-                }
-        }
+            System.out.println("Guess a number between 1 and 10!");
+            while (check){
+                int guess = scanner.nextInt();
+                    if (guess < number){
+                        System.out.println("### The stored number is higher ###");
+                        System.out.println("Guess again!");
+                    } else if (guess > number){
+                        System.out.println("### The stored number is lower ###");
+                        System.out.println("Guess again!");
+                    } else {
+                        System.out.println("##### You found the number: " + number + " #####");
+                        System.out.println("##### Do you want to try again? (type y or n) #####");
+
+                        scanner.nextLine();
+                        String cont = scanner.nextLine();
+
+                        if (cont.equals("n")){
+                            check = false;
+                        }
+                    }
+            }
+        } System.out.println("##### Thanks for playing! #####");
     }
 }
