f0a4b6a0b7b9d4b28f1aad5dac7b9994ec297b4b
Peter Csakvari <csakvaripeter1@gmail.com>
2018-02-16 10:42:44 +0100

    feat(javatasks)DrawTriangle

---
+++ week-01/day-04/src/DrawTriangle.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class DrawTriangle {
+    public static void main(String[] args){
+        // Write a program that reads a number from the standard input, then draws a
+        // triangle like this:
+        //
+        // *
+        // **
+        // ***
+        // ****
+        //
+        // The triangle should have as many lines as the number was
+
+        System.out.println("Please write a number: ");
+        Scanner scanner = new Scanner(System.in);
+        int nmbr = scanner.nextInt();
+        String star = "*";
+
+        for (int i = 0; i < nmbr; i++){
+            System.out.println(star);
+            star += "*";
+        }
+    }
+}
