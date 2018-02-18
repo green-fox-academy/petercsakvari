f96acbb6e6da898c1fc9aa1160e82b2a30e347b1
Peter Csakvari <csakvaripeter1@gmail.com>
2018-02-18 11:06:52 +0100

    fix(javatasks)GuessTheNumber

--- week-01/day-04/src/GuessTheNumber.java
+++ week-01/day-04/src/GuessTheNumber.java
@@ -2,35 +2,45 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class GuessTheNumber {
+
+    static Scanner scanner = new Scanner(System.in);
+    static int guess;
+    static boolean check = true;
+    static String cont = "y";
+
     public static void main(String[] args){
-
-        Scanner scanner = new Scanner(System.in);
-        Random rand = new Random();
-        int number = rand.nextInt(10) + 1;
-        boolean check = true;
-
         while (check){
-            System.out.println("Guess a number between 1 and 10!");
+            Random rand = new Random();
+            int number = rand.nextInt(10) + 1;
             while (check){
-                int guess = scanner.nextInt();
-                    if (guess < number){
-                        System.out.println("### The stored number is higher ###");
-                        System.out.println("Guess again!");
-                    } else if (guess > number){
-                        System.out.println("### The stored number is lower ###");
-                        System.out.println("Guess again!");
-                    } else {
-                        System.out.println("##### You found the number: " + number + " #####");
-                        System.out.println("##### Do you want to try again? (type y or n) #####");
-
-                        scanner.nextLine();
-                        String cont = scanner.nextLine();
-
-                        if (cont.equals("n")){
-                            check = false;
-                        }
-                    }
+                System.out.println("Guess a number between 1 and 100: ");
+                guess = scanner.nextInt();
+                checkGuess(guess, number);
             }
-        } System.out.println("##### Thanks for playing! #####");
+            System.out.println("Thanks for playing!");
+        }
+    }
+
+    public static void checkGuess(int guess, int number){                            //Guess and Number comparing method
+
+            if(guess<number){
+                System.out.println("### The stored number is higher ###");
+            }else if(guess>number){
+                System.out.println("### The stored number is lower ###");
+            } else {
+                System.out.println("##### You found the number: "+number+" #####");
+                System.out.println("##### Do you want to try again? (type y or n) #####");
+                scanner.nextLine();
+                cont = scanner.nextLine();
+                check = checkCont();
+            }
+
+    }
+    public static boolean checkCont () {                        //Check if the player wants to continue
+
+        if (cont.equals("y")) {
+            return true;
+        } else
+            return false;
     }
 }
