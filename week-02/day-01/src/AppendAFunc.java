728add86bac06e9c87a1af37b17a080cbb88b41f
Peter Csakvari <csakvaripeter1@gmail.com>
2018-02-19 12:35:21 +0100

    Feat(JavaTasks)appendAFunc

---
+++ week-02/day-01/src/AppendAFunc.java
@@ -0,0 +1,19 @@
+public class AppendAFunc {
+    //  Create the usual class wrapper
+    //  and main method on your own.
+
+    // - Create a string variable named `am` and assign the value `kuty` to it
+    // - Write a function called `appendA` that gets a string as an input,
+    //   appends an 'a' character to its end and returns with a string
+    //
+    // - Print the result of `appendAFunc(am)`
+    public static void main (String[] args){
+        String am = "kuty";
+        System.out.println(appendA(am));
+
+    }
+    public static String appendA(String input){
+        input = input + "a";
+        return input;
+    }
+}
