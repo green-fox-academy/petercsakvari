4bd23c30795e051d9f7b02987594a1afe0dbab55
Peter Csakvari <csakvaripeter1@gmail.com>
2018-02-19 08:52:28 +0100

    EditedGitignore

---
+++ week-01/day-04/src/Homework.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Homework {
+    public static void main(String[] args){
+        //Asks for a word
+        //Outputs the number of letters in total
+        //Outputs the number of each letter
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please write a word: ");
+
+        Stri
+
+        char abc [] = new char[26];
+
+        char inputWord [] = new char[20];
+}
