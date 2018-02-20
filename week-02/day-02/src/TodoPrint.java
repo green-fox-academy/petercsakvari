32b375b52f63ee52e683ec2e3ba57178ee803e1a
Peter Csakvari <csakvaripeter1@gmail.com>
2018-02-20 12:32:01 +0100

    feat(TodoPrint): Finished

---
+++ week-02/day-02/src/TodoPrint.java
@@ -0,0 +1,21 @@
+public class TodoPrint {
+    public static void main(String[] args) {
+        // Add "My todo:" to the beginning of the todoText
+        // Add " - Download games" to the end of the todoText
+        // Add " - Diablo" to the end of the todoText but with indention
+
+        // Expected outpt:
+
+        // My todo:
+        //  - Buy milk
+        //  - Download games
+        //      - Diablo
+
+
+        String todoText = " - Buy milk\n";
+        todoText = "My todo:\n" + todoText;
+        todoText = todoText + " - Download games\n";
+        todoText = todoText + "\t - Diablo\n";
+        System.out.println(todoText);
+    }
+}
