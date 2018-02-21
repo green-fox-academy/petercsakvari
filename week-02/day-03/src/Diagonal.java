e0918bf51945bfa9fa716fa8784d41f492ea8ab4
Peter Csakvari <csakvaripeter1@gmail.com>
2018-02-21 11:07:56 +0100

    Feat(WorkshopWeek02Day03): Diagonal Finished

---
+++ week-02/day-03/src/Diagonal.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Diagonal {
+    public static void mainDraw(Graphics graphics) {
+        // Draw the canvas' diagonals.
+        // If it starts from the upper-left corner it should be green, otherwise it should be red.
+
+        graphics.setColor(new Color(0, 255, 0));
+        graphics.drawLine(0, 0, HEIGHT, WIDTH);
+
+        graphics.setColor(new Color(255, 0, 0));
+        graphics.drawLine(HEIGHT, 0, 0, WIDTH);
+
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
