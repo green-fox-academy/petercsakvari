b73c391857d0b7efe47303f7ba9f56d3fc6965ad
Peter Csakvari <csakvaripeter1@gmail.com>
2018-02-22 17:20:51 +0100

    Fix(WorkshopWeek02Project): Triangles trying a new way

--- week-02/day-04/src/Triangles.java
+++ week-02/day-04/src/Triangles.java
@@ -1,17 +1,36 @@
 import javax.swing.*;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.*;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class Triangles {
+    static int goDown = 17;
+    static int goSideways = 10;
+    static int nPoints = 3;
 
-    public static void mainDraw(Graphics graphics) {
-        // Main Triangle
-        graphics.drawLine(WIDTH/2, 10, WIDTH/2-140, 250);
-        graphics.drawLine(WIDTH/2, 10, WIDTH/2+140, 250);
-        graphics.drawLine(WIDTH/2-140, 250, WIDTH/2+140, 250);
+    private static void mainDraw(Graphics graphics) {
+        int xCoord = WIDTH / 2;
+        int yCoord = HEIGHT / 6;
 
+        drawFull(graphics, xCoord, yCoord);
+    }
+
+    private static void drawFull(Graphics graphics, int x, int y) {
+        int counter = 1;
+        for (int i = 0; i < 10; i++) {
+            drawTriangle(graphics, counter, x, y);
+        }
+    }
+
+    private static void drawTriangle(Graphics graphics, int counter, int x, int y) {
+        for (counter = 1; counter < 15; counter++) {
+            int xFirstPoints[] = {x - (i*goDown), x - ((i + 1) * goSideways), x + ((i + 1) * goSideways)};
+            int yFirstPoints[] = {y + (i*goDown), y + ((i + 1) * goDown), y + ((i + 1) * goDown)};
+            graphics.drawPolygon(xFirstPoints, yFirstPoints, nPoints);
+        }
     }
 
     // Don't touch the code below
@@ -19,7 +38,7 @@ public class Triangles {
     static int HEIGHT = 343;
 
     public static void main(String[] args) {
-        JFrame jFrame = new JFrame("Line Play");
+        JFrame jFrame = new JFrame("Drawing");
         jFrame.setSize(new Dimension(WIDTH, HEIGHT));
         jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
         jFrame.add(new ImagePanel());
