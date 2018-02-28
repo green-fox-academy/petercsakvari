47bc5b93b1ce9841eb14bded4aff931e8687fe17
Peter Csakvari <csakvaripeter1@gmail.com>
2018-02-28 11:39:45 +0100

    Feat(ObjectOrientedProgramming): BlogPost Finished

---
+++ week-03/day-03/src/BlogPost/BlogPost.java
@@ -0,0 +1,22 @@
+package BlogPost;
+
+//Create a BlogPost class that has
+//        an authorName
+//        a title
+//        a text
+//        a publicationDate
+
+public class BlogPost {
+
+  private String authorName;
+  private String title;
+  private String text;
+  private String publicationDate;
+
+  public BlogPost(String authorName, String title, String text, String publicationDate) {
+    this.authorName = authorName;
+    this.title = title;
+    this.text = text;
+    this.publicationDate = publicationDate;
+  }
+}
