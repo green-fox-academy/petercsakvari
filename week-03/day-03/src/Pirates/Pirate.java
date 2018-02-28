61c8eb52380755903418df972a6b0351af430ceb
Peter Csakvari <csakvaripeter1@gmail.com>
2018-02-28 15:33:20 +0100

    Feat(ObjectOrientedProgramming): Pirates, Pirate class Finished

--- week-03/day-03/src/Pirates/Pirate.java
+++ week-03/day-03/src/Pirates/Pirate.java
@@ -1,22 +1,36 @@
 package Pirates;
 
+import java.util.Random;
+
 public class Pirate {
 
-  private int pirateHealth = 100;
   private boolean isDead = false;
   private int drunkenness = 0;
 
-  public void drinkSomeRum() {
-    if (isDead) {
-      System.out.println("He's dead!");
+  public void brawl (Pirate pirate1, Pirate pirate2) {
+    if (!pirate1.isDead && !pirate2.isDead) {
+      Random random = new Random();
+      int randomWin = random.nextInt(2) + 1;
+      if (randomWin == 1) {
+        System.out.println(pirate1 + " won!");
+      } else if (randomWin == 2) {
+        System.out.println(pirate2 + " won!");
+      } else if (randomWin == 3) {
+        pirate1.passOut();
+        pirate2.passOut();
+      }
+    } else {
+      System.out.println("One of the pirates is dead!");
     }
+  }
+
+  public void drinkSomeRum() {
+    checkIfDead();
     drunkenness++;
   }
 
   public void howsItGoingMate() {
-    if (isDead) {
-      System.out.println("He's dead!");
-    }
+    checkIfDead();
     talk();
   }
 
@@ -24,22 +38,24 @@ public class Pirate {
     isDead = true;
   }
 
-  private void talk() {
+  private void checkIfDead() {
     if (isDead) {
       System.out.println("He's dead!");
     }
+  }
+
+  private void talk() {
+    checkIfDead();
     if (drunkenness >= 0 && drunkenness <= 4) {
       System.out.println("Pirate: Pour me anudder!");
     } else {
       System.out.println("Pirate: Arghh, I'ma Pirate. How d'ya d'ink its goin?");
-      sleep();
+      passOut();
     }
   }
 
-  private void sleep() {
-    if (isDead) {
-      System.out.println("He's dead!");
-    }
+  private void passOut() {
+    checkIfDead();
     System.out.println("* the pirate passes out and sleeps it off *");
   }
 
