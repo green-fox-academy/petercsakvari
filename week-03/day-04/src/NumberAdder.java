aad7dbfd088f18ca122743e63b1c2dad15481e5a
Peter Csakvari <csakvaripeter1@gmail.com>
2018-03-01 10:17:12 +0100

    Feat(Recursion): NumberAdder Finished

---
+++ week-03/day-04/src/NumberAdder.java
@@ -0,0 +1,14 @@
+public class NumberAdder {
+  // Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
+  public static void main(String[] args) {
+    int n = 5;
+    System.out.println(numberAdder(n));
+  }
+  public static int numberAdder (int n) {
+    if (n == 0) {
+      return 0;
+    } else {
+      return n + numberAdder(n - 1);
+    }
+  }
+}
