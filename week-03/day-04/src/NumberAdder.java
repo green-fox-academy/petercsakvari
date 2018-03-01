fd3d253d8e284cd7bcf5c22e2a05c6639e800fa7
Peter Csakvari <csakvaripeter1@gmail.com>
2018-03-01 10:32:38 +0100

    Feat(Recursion): SumDigit Finished

--- week-03/day-04/src/NumberAdder.java
+++ week-03/day-04/src/NumberAdder.java
@@ -1,7 +1,7 @@
 public class NumberAdder {
   // Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
   public static void main(String[] args) {
-    int n = 5;
+    int n = 10;
     System.out.println(numberAdder(n));
   }
   public static int numberAdder (int n) {
