f5ae09ff280089518417ffd2ea8b524dc61731c0
Peter Csakvari <csakvaripeter1@gmail.com>
2018-03-07 11:59:34 +0100

    Feat(Abstracts&Interfaces) Cloneable Finished

---
+++ week-04/day-03/src/cloneable/CloneableMain.java
@@ -0,0 +1,10 @@
+package cloneable;
+
+public class CloneableMain {
+  public static void main(String[] args) {
+
+    Student student1 = new Student("John", 20, "male", "BME");
+    student1.clone(student1);
+
+  }
+}
---
+++ week-04/day-03/src/cloneable/Student.java
@@ -0,0 +1,42 @@
+package cloneable;
+
+public class Student extends Person implements Cloneable {
+
+  private String previousOrganization;
+  private int skippedDays;
+
+  public Student (String name, int age, String gender, String previousOrganization) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.previousOrganization = previousOrganization;
+    skippedDays = 0;
+  }
+
+  public Student () {
+    super();
+    previousOrganization = "The School of Life";
+    skippedDays = 0;
+  }
+
+  public Object clone(Student student) {
+    Student studentClone = student;
+    return studentClone;
+  }
+
+  @Override
+  public void introduce() {
+    System.out.println( "Hi, I'm " + name + ", a " + age + " year old gender from " + previousOrganization + " who " +
+            "skipped " + skippedDays + " days from the course already.");
+  }
+
+  @Override
+  public void getGoal() {
+    System.out.println("Be a junior software developer.");
+  }
+
+  public void skipDays(int numberOfDays) {
+    skippedDays += numberOfDays;
+  }
+
+}
