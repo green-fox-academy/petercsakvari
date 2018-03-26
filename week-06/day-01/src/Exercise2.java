3f2205d41c80306bd93a9a80bda84a2a0e6523b4
Peter Csakvari <csakvaripeter1@gmail.com>
2018-03-26 11:57:02 +0200

    feat(Streams) exercise5 finished

--- week-06/day-01/src/Exercise2.java
+++ week-06/day-01/src/Exercise2.java
@@ -1,14 +1,13 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Optional;
 import java.util.OptionalDouble;
 
 public class Exercise2 {
   public static void main(String[] args) {
     ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
     OptionalDouble average = numbers.stream()
-            .filter(n -> n % 2 == 1)
-            .mapToInt(n -> n)
+            .filter(n -> n % 2 != 0)
+            .mapToDouble(n -> n)
             .average();
     double averageD = average.orElse(0);
     System.out.println(averageD);
