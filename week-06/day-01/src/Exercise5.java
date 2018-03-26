8e2ce1a32cee96fa86e15e2b3ae03e7552ec1378
Peter Csakvari <csakvaripeter1@gmail.com>
2018-03-26 12:52:47 +0200

    refact(Streams) exercise5 refactored

--- week-06/day-01/src/Exercise5.java
+++ week-06/day-01/src/Exercise5.java
@@ -1,16 +1,14 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Exercise5 {
   public static void main(String[] args) {
     //Write a Stream Expression to find the uppercase characters in a string!
-    String s = "sdasSADDASDsadasdASdADSA";
-    List<Character> letters = new ArrayList<>();
-    for (int i = 0; i < s.length(); i++) {
-      letters.add(s.charAt(i));
-    }
-    letters.stream()
+    String s = "sdasSADDASDsa dasdASdADSA";
+    s.chars()
+            .mapToObj(c -> (char) c)
             .filter(Character::isUpperCase)
             .forEach(System.out::print);
   }
