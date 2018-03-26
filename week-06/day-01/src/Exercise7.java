0ef1a7320b94017d8ee5e122e13a62c53aaf79ae
Peter Csakvari <csakvaripeter1@gmail.com>
2018-03-26 12:54:56 +0200

    refact(Streams) exercise7 refactored

--- week-06/day-01/src/Exercise7.java
+++ week-06/day-01/src/Exercise7.java
@@ -6,11 +6,8 @@ public class Exercise7 {
   public static void main(String[] args) {
     //Write a Stream Expression to find the frequency of characters in a given string!
     String s = "smdfhflkyfhdfkiofdoisusfdsfdaksdafgh";
-    List<Character> characters = new ArrayList<>();
-    for (int i = 0; i < s.length(); i++) {
-      characters.add(s.charAt(i));
-    }
-    System.out.println(characters.stream()
+    System.out.println(s.chars()
+            .mapToObj(c -> (char) c)
             .collect(Collectors.groupingBy(c -> c, Collectors.counting())));
   }
 }
