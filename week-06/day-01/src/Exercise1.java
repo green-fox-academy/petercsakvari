5ae7d55d85f2b170993b5651c764b900c9d2aa27
Peter Csakvari <csakvaripeter1@gmail.com>
2018-03-26 11:25:57 +0200

    feat(Streams) exercise1 finished

---
+++ week-06/day-01/src/Exercise1.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Exercise1 {
+  public static void main(String[] args) {
+    ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
+    List<Integer> evenNumbers = numbers
+            .stream()
+            .filter(n -> n % 2 == 0)
+            .sorted()
+            .collect(Collectors.toList());
+    System.out.println(evenNumbers);
+  }
+}
