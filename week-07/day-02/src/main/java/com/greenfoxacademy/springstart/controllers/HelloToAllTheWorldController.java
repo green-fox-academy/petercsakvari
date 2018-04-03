d7459cff205a78ebb11e5b723187ee29c68e6f2a
Peter Csakvari <csakvaripeter1@gmail.com>
2018-04-03 17:04:09 +0200

    feat(SpringStart): hello to all world listing and color

---
+++ week-07/day-02/src/main/java/com/greenfoxacademy/springstart/controllers/HelloToAllTheWorldController.java
@@ -0,0 +1,29 @@
+package com.greenfoxacademy.springstart.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.awt.*;
+import java.util.Random;
+
+@Controller
+public class HelloToAllTheWorldController {
+
+  private static final String[] HELLOS = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+          "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+          "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+          "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+
+  @RequestMapping("/web/greetingworld")
+  public String greeting(Model model) {
+    Random random = new Random();
+    int nextInt = random.nextInt(256*256*256);
+    String colorCode = String.format("#%06x", nextInt);
+
+    model.addAttribute("hello", HELLOS);
+    model.addAttribute("randColor", colorCode);
+
+    return "hellotoall";
+  }
+}
