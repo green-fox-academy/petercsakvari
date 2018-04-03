7f44a3fb80aadcc50dcf5dd741cadc355ee58822
Peter Csakvari <csakvaripeter1@gmail.com>
2018-04-03 14:31:24 +0200

    feat(SpringStart): web greet app

--- week-07/day-02/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-07/day-02/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -3,13 +3,15 @@ package com.greenfoxacademy.springstart.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWebController {
 
-  @RequestMapping("/web/greeting")
-  public String greeting(Model model) {
-    model.addAttribute("name", " World");
+  @RequestMapping(name = "/web/greeting", method = RequestMethod.GET)
+  public String greeting(@RequestParam String name, Model model) {
+    model.addAttribute("name", name);
     return "greeting";
   }
 }
