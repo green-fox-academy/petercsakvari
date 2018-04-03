4d4a6f507a85275ec0eeea98e9875cdf5f5e2a14
Peter Csakvari <csakvaripeter1@gmail.com>
2018-04-03 12:43:40 +0200

    feat(SpringStart): hello user rest app

--- week-07/day-02/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-07/day-02/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -2,13 +2,15 @@ package com.greenfoxacademy.springstart.controllers;
 
 import com.greenfoxacademy.springstart.Greeting;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
 
-  @RequestMapping("/greeting")
-  public Greeting greeting() {
-    return new Greeting(1, "Hola");
+  @RequestMapping(value = "/greeting", method = RequestMethod.GET)
+  public Greeting greeting(@RequestParam String name) {
+    return new Greeting(1, "Hola " + name);
   }
 }
