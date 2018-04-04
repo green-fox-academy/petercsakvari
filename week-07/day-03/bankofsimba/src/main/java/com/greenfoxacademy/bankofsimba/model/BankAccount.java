ea283407fc08534e3831a1797a66214510b320d6
Peter Csakvari <csakvaripeter1@gmail.com>
2018-04-04 12:35:49 +0200

    feat(bankofsimba): bank account show with currency and decimals

--- week-07/day-03/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/model/BankAccount.java
+++ week-07/day-03/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/model/BankAccount.java
@@ -1,26 +1,36 @@
 package com.greenfoxacademy.bankofsimba.model;
 
+import java.text.DecimalFormat;
+
 public class BankAccount {
 
+  private static final String CURRENCY = "ZEB";
+  private static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("#.00");
   private String name;
-  private int balance;
+  private double balance;
+  private String balanceWithCurrency;
   private String animalType;
 
-  public BankAccount(String name, int balance, String animalType) {
+  public BankAccount(String name, double balance, String animalType) {
     this.name = name;
     this.balance = balance;
     this.animalType = animalType;
+    balanceWithCurrency = DECIMAL_FORMAT.format(this.balance) + " " + CURRENCY;
   }
 
   public String getName() {
     return name;
   }
 
-  public int getBalance() {
+  public double getBalance() {
     return balance;
   }
 
   public String getAnimalType() {
     return animalType;
+  }
+
+  public String getBalanceWithCurrency() {
+    return balanceWithCurrency;
   }
 }
