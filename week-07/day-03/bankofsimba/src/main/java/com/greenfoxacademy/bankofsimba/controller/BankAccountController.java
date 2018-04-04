d55fe5461541988ba96c1a475c589696c5172fb8
Peter Csakvari <csakvaripeter1@gmail.com>
2018-04-04 17:32:06 +0200

    init(bankofsimba): submit form init

--- week-07/day-03/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/controller/BankAccountController.java
+++ week-07/day-03/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/controller/BankAccountController.java
@@ -3,26 +3,29 @@ package com.greenfoxacademy.bankofsimba.controller;
 import com.greenfoxacademy.bankofsimba.model.BankAccount;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class BankAccountController {
 
-  @RequestMapping("/showaccounts")
+  @GetMapping("/showaccounts")
   public String showAccount(Model model) {
-
     List<BankAccount> bankAccountList = new ArrayList<>();
     bankAccountList.add(new BankAccount("Simba", 9850, "lion", true, true));
     bankAccountList.add(new BankAccount("Rafiki", 3100, "baboon",true));
     bankAccountList.add(new BankAccount("Scar", 5450, "lion",false));
     bankAccountList.add(new BankAccount("Pumba", 2400, "boar",true));
     bankAccountList.add(new BankAccount("Zazu", 3900, "bird",true));
-
     model.addAttribute("bankAccList", bankAccountList);
     return "bankaccount";
+  }
+
+  @PostMapping("/showaccounts")
+  public String balanceRaise() {
+    return "raisesuccess";
   }
 }
