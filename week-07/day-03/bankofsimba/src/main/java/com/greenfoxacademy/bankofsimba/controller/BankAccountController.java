3a1475a12ad423e576f81202d99c4b62298b48ae
Peter Csakvari <csakvaripeter1@gmail.com>
2018-04-04 12:52:35 +0200

    feat(bankofsimba): fill and show multiple accounts

--- week-07/day-03/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/controller/BankAccountController.java
+++ week-07/day-03/bankofsimba/src/main/java/com/greenfoxacademy/bankofsimba/controller/BankAccountController.java
@@ -5,13 +5,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class BankAccountController {
 
-  @RequestMapping("/show")
+  @RequestMapping("/showaccounts")
   public String showAccount(Model model) {
-    BankAccount bankAccount = new BankAccount("Simba", 2000, "lion");
-    model.addAttribute("bankAcc", bankAccount);
+
+    List<BankAccount> bankAccountList = new ArrayList<>();
+    bankAccountList.add(new BankAccount("Simba", 2000, "lion"));
+    bankAccountList.add(new BankAccount("Rafiki", 2000, "baboon"));
+    bankAccountList.add(new BankAccount("Scar", 2000, "lion"));
+    bankAccountList.add(new BankAccount("Pumbaa", 2000, "boar"));
+    bankAccountList.add(new BankAccount("Zazu", 2000, "bird"));
+
+    model.addAttribute("bankAccList", bankAccountList);
     return "bankaccount";
   }
 }
