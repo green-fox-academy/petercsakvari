f7d78dc20e242c8d04d61ca0a416e6623233c535
Peter Csakvari <csakvaripeter1@gmail.com>
2018-04-05 11:38:43 +0200

    feat(dependencies) hello bean world

--- week-07/day-04/dependencies/src/main/java/com/greenfoxacademy/dependencies/DependenciesApplication.java
+++ week-07/day-04/dependencies/src/main/java/com/greenfoxacademy/dependencies/DependenciesApplication.java
@@ -1,12 +1,26 @@
 package com.greenfoxacademy.dependencies;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DependenciesApplication {
+public class DependenciesApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DependenciesApplication.class, args);
-	}
+  private Printer printer;
+
+  @Autowired
+  public DependenciesApplication(Printer printer) {
+    this.printer = printer;
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(DependenciesApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    System.out.println(printer.log("This no!"));
+  }
 }
---
+++ week-07/day-04/dependencies/src/main/java/com/greenfoxacademy/dependencies/Printer.java
@@ -0,0 +1,16 @@
+package com.greenfoxacademy.dependencies;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class Printer {
+
+  public Printer() {
+  }
+
+  public String log(String message) {
+    return LocalDateTime.now() + " MY PRINTER SAYS --- " + message;
+  }
+}
