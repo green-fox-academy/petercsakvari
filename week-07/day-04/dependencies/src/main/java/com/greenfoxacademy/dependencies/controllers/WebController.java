efdaf1035a40d8ca68987e815209d64fbe74159d
Peter Csakvari <csakvaripeter1@gmail.com>
2018-04-05 16:53:19 +0200

    refact(dependencies): email validation controller refactor

--- week-07/day-04/dependencies/src/main/java/com/greenfoxacademy/dependencies/controllers/WebController.java
+++ week-07/day-04/dependencies/src/main/java/com/greenfoxacademy/dependencies/controllers/WebController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WebController {
@@ -25,5 +26,12 @@ public class WebController {
   public String colorBackground(Model model) {
     model.addAttribute("randColor", utilityService.randomColor());
     return "colored";
+  }
+
+  @GetMapping("/useful/email")
+  public String checkEmail(@RequestParam(name = "email", required = false) String email, Model model) {
+    model.addAttribute("email", email);
+    model.addAttribute("isValid", utilityService.validateEmail(email));
+    return "email_validation";
   }
 }
