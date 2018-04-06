84ba3ff461c27e88f6f28c36bfd2a598fe47e7b5
Peter Csakvari <csakvaripeter1@gmail.com>
2018-04-06 10:01:50 +0200

    feat(foxclub): login page implement

--- week-07/day-05/foxclubproject/src/main/java/com/greenfoxacademy/foxclubproject/controllers/MainController.java
+++ week-07/day-05/foxclubproject/src/main/java/com/greenfoxacademy/foxclubproject/controllers/MainController.java
@@ -1,13 +1,35 @@
 package com.greenfoxacademy.foxclubproject.controllers;
 
+import com.greenfoxacademy.foxclubproject.models.Fox;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
+  private Fox fox;
+
+  @Autowired
+  public MainController(Fox fox) {
+    this.fox = fox;
+  }
+
   @GetMapping("/")
   public String showIndex() {
     return "index";
+  }
+
+  @GetMapping("/login")
+  public String showLogin() {
+    return "login";
+  }
+
+  @PostMapping("/login")
+  public String login(@ModelAttribute(name = "name") String name) {
+    return "login";
   }
 }
