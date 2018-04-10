979eb982b3f84dbdeea8dee09c34723727cec8f4
Peter Csakvari <csakvaripeter1@gmail.com>
2018-04-10 17:21:50 +0200

    refact(todomysql): todo controller delete unused imports

--- week-08/day-02/todowithmysql/src/main/java/com/greenfoxacademy/todowithmysql/controllers/TodoController.java
+++ week-08/day-02/todowithmysql/src/main/java/com/greenfoxacademy/todowithmysql/controllers/TodoController.java
@@ -1,18 +1,13 @@
 package com.greenfoxacademy.todowithmysql.controllers;
 
-import com.greenfoxacademy.todowithmysql.models.Todo;
-import com.greenfoxacademy.todowithmysql.repositories.TodoRepository;
 import com.greenfoxacademy.todowithmysql.services.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/todo")
