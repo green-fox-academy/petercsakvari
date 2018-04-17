4787882d232576f5937cd6fc125a4b731c4c825d
Peter Csakvari <csakvaripeter1@gmail.com>
2018-04-17 11:31:26 +0200

    init(restprojectv2): project init

---
+++ week-09/day-02/restv2/src/main/java/com/greenfoxacademy/restv2/Restv2Application.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.restv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Restv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Restv2Application.class, args);
+	}
+}
