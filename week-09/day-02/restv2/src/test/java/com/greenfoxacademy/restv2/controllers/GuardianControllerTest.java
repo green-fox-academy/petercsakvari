64f51036fc8a761174bcfb72c91e17cd1a0c7061
Peter Csakvari <csakvaripeter1@gmail.com>
2018-04-17 16:09:46 +0200

    refact(restv2): refactor controller test delete unused import

--- week-09/day-02/restv2/src/test/java/com/greenfoxacademy/restv2/controllers/GuardianControllerTest.java
+++ week-09/day-02/restv2/src/test/java/com/greenfoxacademy/restv2/controllers/GuardianControllerTest.java
@@ -6,14 +6,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import java.nio.charset.Charset;
 
 import static org.hamcrest.core.Is.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
