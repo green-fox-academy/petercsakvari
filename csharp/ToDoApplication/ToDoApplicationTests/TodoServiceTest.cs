bc73b699f08f0a07847cd536d90a9a03bca2307e
Peter Csakvari <csakvaripeter1@gmail.com>
2018-04-25 20:42:12 +0200

    feat(cstodoapp): service GetTodoById test

--- csharp/ToDoApplication/ToDoApplicationTests/TodoServiceTest.cs
+++ csharp/ToDoApplication/ToDoApplicationTests/TodoServiceTest.cs
@@ -4,26 +4,41 @@ using Moq;
 using ToDoApplication.Repositories;
 using ToDoApplication.Services;
 using ToDoApplication.models;
+using Newtonsoft.Json;
 
 namespace ToDoApplicationTests
 {
     public class TodoServiceTest
     {
-        [Fact]        
+        [Fact]
         public void EditByIdTest()
         {
-            var mockTodoRepository = new Mock<ITodoRepository>();
-            mockTodoRepository.Setup(x => x.UpdateTodo(It.IsAny<Todo>()));
+            var mockTodoRepository = new Mock<ITodoRepository>(MockBehavior.Strict);
+            mockTodoRepository.Setup(r => r.UpdateTodo(null)).Throws(new Exception());
+            mockTodoRepository.Setup(t => t.UpdateTodo(It.IsAny<Todo>()));
             var todoService = new TodoService(mockTodoRepository.Object);
 
-            Assert.NotNull(todoService);
-
-            var desiredParam = new Todo("test");
-
-            todoService.EditById(desiredParam);
+            todoService.EditById(new Todo());
 
             Assert.Throws<Exception>(() => todoService.EditById(null));
-            mockTodoRepository.Verify(x => x.UpdateTodo(desiredParam), Times.Once());
+            mockTodoRepository.Verify(r => r.UpdateTodo(It.IsAny<Todo>()), Times.Once);
+        }
+
+        [Fact]
+        public void GetTodoByIdTest()
+        {
+            var mockTodoRepository = new Mock<ITodoRepository>();
+            var expected = new Todo("test");
+            mockTodoRepository.Setup(r => r.FindById(0)).Returns(expected);
+            var todoService = new TodoService(mockTodoRepository.Object);
+
+            var result = todoService.GetTodoById(0);
+
+            var expectedStr = JsonConvert.SerializeObject(expected);
+            var resultStr = JsonConvert.SerializeObject(result);
+
+            mockTodoRepository.Verify(x => x.FindById(It.IsAny<long>()), Times.Once());
+            Assert.Equal(expected, result);
         }
     }
 }
